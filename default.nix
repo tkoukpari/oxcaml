f0e8ebbebce6da0e47860c611545fe52e385027c
George Shammas <georgyo@gmail.com>
2025-10-07 13:22:16 -0400

    Nix flake github-actions (#4711)

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
   addressSanitizer ? false,
   dev ? false,
   flambdaInvariants ? false,
-  framePointers ? false,
+  framePointers ? addressSanitizer,
   multidomain ? false,
   ocamltest ? true,
   pollInsertion ? false,
@@ -181,22 +181,21 @@ stdenv.mkDerivation {
   # step, which expects --libdir to be $out/lib/ocaml
   setOutputFlags = false;
 
-  nativeBuildInputs =
-    [
-      pkgs.autoconf
-      menhir
-      ocaml
-      dune
-      pkgs.pkg-config
-      pkgs.rsync
-      pkgs.which
-      pkgs.parallel
-      gfortran # Required for Bigarray Fortran tests
-      upstream.ocamlformat_0_24_1 # required for make fmt
-      pkgs.removeReferencesTo
-    ]
-    ++ (if pkgs.stdenv.isDarwin then [ pkgs.cctools ] else [ pkgs.libtool ]) # cctools provides Apple libtool on macOS
-    ++ lib.optional oxcamlLldb pkgs.python312;
+  nativeBuildInputs = [
+    pkgs.autoconf
+    menhir
+    ocaml
+    dune
+    pkgs.pkg-config
+    pkgs.rsync
+    pkgs.which
+    pkgs.parallel
+    gfortran # Required for Bigarray Fortran tests
+    upstream.ocamlformat_0_24_1 # required for make fmt
+    pkgs.removeReferencesTo
+  ]
+  ++ (if pkgs.stdenv.isDarwin then [ pkgs.cctools ] else [ pkgs.libtool ]) # cctools provides Apple libtool on macOS
+  ++ lib.optional oxcamlLldb pkgs.python312;
 
   buildInputs = [
     pkgs.llvm # llvm-objcopy is used for debuginfo
@@ -252,4 +251,8 @@ stdenv.mkDerivation {
       make test-one TEST=...   - Run a single test
     EOF
   '';
+
+  meta =
+    { } // (if framePointers && !pkgs.stdenv.hostPlatform.isx86_64 then { broken = true; } else { });
+
 }
---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "OxCaml - A performance-focused fork of OCaml";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nix-github-actions,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+        oxcaml = pkgs.callPackage ./default.nix { src = self; };
+      in
+      {
+        packages = {
+          inherit oxcaml;
+          oxcaml-fp = oxcaml.override { framePointers = true; };
+          oxcaml-r5 = oxcaml.override { runtime5 = true; };
+          oxcaml-fp-r5 = oxcaml.override {
+            framePointers = true;
+            runtime5 = true;
+          };
+          oxcaml-asan = oxcaml.override { addressSanitizer = true; };
+          oxcaml-asan-r5 = oxcaml.override {
+            addressSanitizer = true;
+            runtime5 = true;
+          };
+          default = oxcaml;
+        };
+
+        checks = lib.attrsets.filterAttrs (key: drv: !(drv.meta.broken or false)) {
+          inherit (self.packages.${system})
+            oxcaml
+            oxcaml-fp
+            oxcaml-r5
+            oxcaml-fp-r5
+            oxcaml-asan
+            oxcaml-asan-r5
+            ;
+        };
+
+        formatter = pkgs.nixfmt-tree;
+
+        devShells.default = self.packages.${system}.oxcaml;
+
+      }
+    )
+    // {
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = nixpkgs.lib.getAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ] self.checks;
+      };
+    };
+}
