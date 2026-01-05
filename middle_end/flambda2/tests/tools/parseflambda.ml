2c3e0c4f96fab700a01d6cdc7b7d055a5f8938b6
Basile Clément <129742207+bclement-ocp@users.noreply.github.com>
2026-01-05 13:08:08 +0100

    Fix compilation of parseflambda tool (#5196)

--- middle_end/flambda2/tests/tools/parseflambda.ml
+++ middle_end/flambda2/tests/tools/parseflambda.ml
@@ -26,6 +26,7 @@ let parse_flambda filename =
        code_slot_offsets here (as well as free_names) *)
     let { Simplify.unit = fl2'; _ } =
       Simplify.run ~cmx_loader ~round:1 fl2 ~code_slot_offsets:Code_id.Map.empty
+        ~machine_width:Sixty_four
     in
     Format.printf "simplify:@.%a@." Flambda_unit.print fl2';
     let fl3 = Flambda_to_fexpr.conv fl2' in
