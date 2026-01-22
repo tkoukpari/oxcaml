8f2f32637095277d58975f8160b53fb5f30eea2c
Basile Clément <129742207+bclement-ocp@users.noreply.github.com>
2026-01-22 10:42:45 +0100

    chamelon: Handle submodules and signatures in `deletelines` (#5294)

--- chamelon/minimizer/deletelines.ml
+++ chamelon/minimizer/deletelines.ml
@@ -25,28 +25,46 @@
  * DEALINGS IN THE SOFTWARE.                                                  *
  ******************************************************************************)
 
-(* Minimizer del-line: delete the last definitions *)
+(* Minimizer del-line: delete the last definitions.
+
+   This minimizer remove structure items and signature items, recursing into the
+   remaining items to handle submodules.
+
+   Definitions/declarations are processed in reverse order so that we remove
+   as many dependencies as possible before processing an item.
+ *)
 
 open Typedtree
 open Utils
 
-let rec remove_last = function
-  | [] -> []
-  | [ _ ] -> []
-  | h :: q -> h :: remove_last q
-
 let minimize should_remove map cur_file =
-  let str = Smap.find cur_file map in
-  let nstr =
+  let mapper =
     {
-      str with
-      str_items =
-        List.rev
-          (List.filter
-             (fun _ -> not (should_remove ()))
-             (List.rev str.str_items));
+      Tast_mapper.default with
+      structure =
+        (fun sub str ->
+          let rev_str_items =
+            List.filter_map
+              (fun str_item ->
+                if should_remove () then None
+                else Some (sub.structure_item sub str_item))
+              (List.rev str.str_items)
+          in
+          { str with str_items = List.rev rev_str_items });
+      signature =
+        (fun sub signature ->
+          let rev_sig_items =
+            List.filter_map
+              (fun sig_item ->
+                if should_remove () then None
+                else Some (sub.signature_item sub sig_item))
+              (List.rev signature.sig_items)
+          in
+          { signature with sig_items = List.rev rev_sig_items });
     }
   in
+  let str = Smap.find cur_file map in
+  let nstr = mapper.structure mapper str in
   Smap.add cur_file nstr map
 
 let minimizer = { minimizer_name = "delete-lines"; minimizer_func = minimize }
